652363c9dbd22058cdfb8a13f076f727ae87a534
Manfred Roiger <manfred.roiger@gmail.com>
2020-01-04 17:25:36 +0100

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Manfred Roiger on 04.01.20.
+//  Copyright © 2020 Manfred Roiger. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
